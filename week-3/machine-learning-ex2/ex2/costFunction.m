6982e79fe278341541e42f94af7ea81d48f408eb
Rustam-Z <zokirovrustam202@gmail.com>
2020-08-26 22:29:05 +0500

    week-3 finished

--- week-3/machine-learning-ex2/ex2/costFunction.m
+++ week-3/machine-learning-ex2/ex2/costFunction.m
@@ -20,11 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X*theta); % hypothesis
 
+J = (-1 / m) * ((y' * log(h)) + (1 - y)' * log(1 - h));
 
-
-
-
+grad = (1/m) * (h - y)' * X; 
 
 
 % =============================================================
--- week-3/machine-learning-ex2/ex2/costFunctionReg.m
+++ week-3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,6 +17,14 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta); % hypothesis
+
+J = (-1 / m) * ((y' * log(h)) + (1 - y)' * log(1 - h)) + (lambda/(2*m)) * (theta(2:length(theta)))' * theta(2:length(theta));
+
+thetaZero = theta;
+thetaZero(1) = 0;
+
+grad = ((1 / m) * (h - y)' * X) + lambda / m * thetaZero';
 
 
 
